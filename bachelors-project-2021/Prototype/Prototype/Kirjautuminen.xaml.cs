4d29309cebe43862f5f5ba7b55a78df95da0272d
Pekka-PC <jtyni18@student.oulu.fi>
2022-04-01 13:31:04 +0300

    Kirjautuminen lisätty

---
+++ bachelors-project-2021/Prototype/Prototype/Kirjautuminen.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Prototype
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Kirjautuminen : ContentPage
+    {
+        public Kirjautuminen()
+        {
+            InitializeComponent();
+        }
+
+        private void MyClick(object sender, EventArgs e)
+        {
+            if(txtUsername.Text =="opettaja" && txtPassword.Text == "opehuone")
+            { 
+            
+            }
+            else
+            {
+                DisplayAlert("VIRHE", "Vaara salasana tai kayttajatunnus", "OK");
+            }
+        }
+    }
+}
