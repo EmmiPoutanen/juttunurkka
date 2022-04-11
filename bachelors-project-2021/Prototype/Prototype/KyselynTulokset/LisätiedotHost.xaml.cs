2289943ee389c071bcb320b8fd6fed6b489ba14b
Pauliina Pihlajaniemi <epihlaja18@student.oulu.fi>
2022-04-11 16:00:26 +0300

    Lisätty destroyclient keskeytäclickediin.

--- bachelors-project-2021/Prototype/Prototype/KyselynTulokset/LisätiedotHost.xaml.cs
+++ bachelors-project-2021/Prototype/Prototype/KyselynTulokset/LisätiedotHost.xaml.cs
@@ -1,31 +1,23 @@
-﻿
-/*
+﻿/*
 Copyright 2021 Emma Kemppainen, Jesse Huttunen, Tanja Kultala, Niklas Arjasmaa
-
 This file is part of "Mieliala kysely".
-
 Mieliala kysely is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, version 3 of the License.
-
 Mieliala kysely is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
-
 You should have received a copy of the GNU General Public License
 along with Mieliala kysely.  If not, see <https://www.gnu.org/licenses/>.
 */
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-
 namespace Prototype
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -36,7 +28,6 @@ namespace Prototype
         public string introMessage { get; set; }
         public IList<double> resultScale { get; set; }
         public IList<int> resultAmount { get; set; }
-
         public class CollectionItem
         {
             public Emoji Item { get; set; } = null;
@@ -47,20 +38,17 @@ namespace Prototype
             resultImages = new List<string>();
             resultScale = new List<double>();
             resultAmount = new List<int>();
-
             Survey s = SurveyManager.GetInstance().GetSurvey();
             introMessage += s.introMessage;
-
             Emojit = new List<CollectionItem>();
             List<Emoji> temp = s.emojis;
-
             foreach (var item in temp)
             {
                 CollectionItem i = new CollectionItem();
                 i.Item = item;
                 Emojit.Add(i);
             }
-            Console.WriteLine("emojeita listassa lisätiedothost.cs:ssä: " +s.emojis.Count);
+
             int count = 0;
             double calculateScale = 0.0;
             Dictionary<int, int> sorted = new Dictionary<int, int>();
@@ -73,7 +61,7 @@ namespace Prototype
             foreach (int key in sorted.Keys)
             {
                 resultImages.Add("emoji" + key.ToString() + "lowres.png");
-                Console.WriteLine(key.ToString());
+                Console.WriteLine(key.ToString() + "ja resultimagesissa nyt: " + resultImages.Count);
             }
             foreach (int value in sorted.Values)
             {
@@ -91,23 +79,28 @@ namespace Prototype
         }
         async void KeskeytäClicked(object sender, EventArgs e)
         {
-            //Sulkee kyselyn kaikilta osallistujilta
-
+            //Sulkee kyselyn kaikilta osallisujilta (linjat poikki höhö XD)
             var res = await DisplayAlert("Haluatko varmasti sulkea huoneen?", "", "Kyllä", "Ei");
 
             if (res == true)
             {
+                if (Main.GetInstance().state == Main.MainState.Participating)
+                {
+                    Main.GetInstance().client.DestroyClient();
+                }
+                else
+                {
+                    Main.GetInstance().host.DestroyHost();
+                }
                 await Navigation.PopToRootAsync();
             }
             else return;
-
         }
         async void JatkaClicked(object sender, EventArgs e)
         {
             //Siirrytään odottamaan äänestyksen tuloksia (HOST)
             await Navigation.PushAsync(new TulostenOdotus());
-
-            //Hox, Clientin pitää päästä vastaamaan kyselyyn, ei hostin
+            //Hox, Clientin pitää päästä vastaamaan kyselyy, ei hostin
         }
     }
 }
