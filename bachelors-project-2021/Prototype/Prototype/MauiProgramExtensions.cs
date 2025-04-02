c569c8b9b85614d61a52afac41786d5c825e1680
EmmiPoutanen <emmi.poutanen@hotmail.com>
2025-04-02 18:20:11 +0300

    Some changes to client side UI
    Updated EmojinValinta -view according to new UI design. The counter for
    the answers is not implemented yet. Currently it is not possible to test
    with more than two emojis, so I wasn't able to test how the UI behaves
    then.
    
    Added fonts that are used in the new UI design.
    
    Client results -view was updated. It now displays the results in graph
    but the graph is upside down. Now the results view also triggers the client
    to listen for the activity voting.

--- bachelors-project-2021/Prototype/Prototype/MauiProgramExtensions.cs
+++ bachelors-project-2021/Prototype/Prototype/MauiProgramExtensions.cs
@@ -12,7 +12,9 @@ public static class MauiProgramExtensions
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+                fonts.AddFont("BebasNeue-Regular.ttf", "BebasNeue");
+				fonts.AddFont("Roboto-Medium.ttf", "RobotoMedium");
+            });
 
 #if DEBUG
 		builder.Logging.AddDebug();
