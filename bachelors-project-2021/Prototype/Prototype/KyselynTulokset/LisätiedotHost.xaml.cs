c5a6265489fecca55b0fdd0ee7790af0c5521a0a
Riina <kaipia.riina@gmail.com>
2025-05-08 21:20:24 +0300

    Removed gray bottom border of top bar and updated copyright in views

--- bachelors-project-2021/Prototype/Prototype/KyselynTulokset/LisätiedotHost.xaml.cs
+++ bachelors-project-2021/Prototype/Prototype/KyselynTulokset/LisätiedotHost.xaml.cs
@@ -1,6 +1,7 @@
 ﻿/*
 Copyright 2021 Emma Kemppainen, Jesse Huttunen, Tanja Kultala, Niklas Arjasmaa
           2022 Pauliina Pihlajaniemi, Viola Niemi, Niina Nikki, Juho Tyni, Aino Reinikainen, Essi Kinnunen
+          2025 Joni Lapinkoski
 
 This file is part of "Juttunurkka".
 
